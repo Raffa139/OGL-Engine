0e9290b1a879cf39fb4cffe468c9d1adc065b4ff
Raffael E <raffa_e@web.de>
2023-03-18 11:31:05 +0100

    Prepare annotation to handle gl state

---
+++ engine/src/main/java/de/ren/ecs/engine/state/UseGLState.java
@@ -0,0 +1,6 @@
+package de.ren.ecs.engine.state;
+
+public @interface UseGLState {
+  // https://www.baeldung.com/java-dynamic-proxies
+  // https://www.baeldung.com/java-scan-annotations-runtime
+}
