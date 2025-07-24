154c7ce5fffd97307a6110dde14da8266ec962af
Raffael E <raffa_e@web.de>
2025-07-24 12:34:42 +0200

    Create file system for resources pulled from imported jars

--- engine/src/main/java/de/ren/ogl/engine/util/ResourceLoader.java
+++ engine/src/main/java/de/ren/ogl/engine/util/ResourceLoader.java
@@ -2,15 +2,13 @@ package de.ren.ogl.engine.util;
 
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.FileSystem;
+import java.util.HashMap;
 
 @Component
 public final class ResourceLoader {
@@ -22,21 +20,50 @@ public final class ResourceLoader {
     }
 
     try {
-      return new Resource(url.toURI());
+      URI uri = url.toURI();
+
+      if (uri.getScheme().equals("jar")) {
+        return fromJar(uri);
+      }
+
+      return new Resource(null, uri);
     } catch (URISyntaxException e) {
       throw new FileNotFoundException(file);
     }
   }
 
+  private Resource fromJar(URI uri) {
+    String[] uriParts = uri.toString().split("!");
+    URI jarUri = URI.create(uriParts[0]);
+    URI resourceUri = URI.create(uriParts[1]);
+
+    FileSystem fs;
+    try {
+      fs = FileSystems.newFileSystem(jarUri, new HashMap<>());
+    } catch (FileSystemAlreadyExistsException e) {
+      fs = FileSystems.getFileSystem(jarUri);
+    } catch (IOException e) {
+      throw new UncheckedIOException(String.format("Failed to create of find file system for JAR %s", jarUri), e);
+    }
+
+    return new Resource(fs, resourceUri);
+  }
+
   public static class Resource {
+    private final FileSystem fs;
+
     private final URI uri;
 
-    private Resource(URI uri) {
+    private Resource(FileSystem fs, URI uri) {
+      this.fs = fs;
       this.uri = uri;
     }
 
     public Path toPath() {
-      // TODO: This causes a FileSystemNotFoundException if imported module uses it to get some resources
+      if (fs != null) {
+        return fs.getPath(uri.toString());
+      }
+
       return Paths.get(uri);
     }
 
